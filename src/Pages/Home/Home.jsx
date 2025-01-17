57b6fac5e295f2d028cc8ee83be25ecf330b3717
rasel3980 <rkrasel3980@gmail.com>
2025-01-17 12:30:12 +0600

    added a banner in home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import Banner from "../../Components/Banner";
 
 const Home = () => {
     return (
         <div>
-            This is Home page
+            <Banner></Banner>
         </div>
     );
 };
